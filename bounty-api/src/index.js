919bfa71dbea27748868e510367abc69a0013313
endikaaryandhi <endikaaryandhi15@gmail.com>
2025-11-19 02:24:55 +0700

    Perbaikan bug pada index.js

--- bounty-api/src/index.js
+++ bounty-api/src/index.js
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
     origin: '*', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 app.use(express.json());
